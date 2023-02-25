f7b41d096b61d0d357265fa9eba08db311193536
superboot <superboot@pm.me>
2023-02-24 19:04:44 -0500

    Add F9 keybinding to reload Awesome rc.lua file, to keep consistent with VIM and TMUX.

--- rc.lua
+++ rc.lua
@@ -595,7 +595,9 @@ root.buttons(gears.table.join(
                 {description = "open a terminal", group = "launcher"}),
         awful.key({ modkey, "Control" }, "r", awesome.restart,
                 {description = "reload awesome", group = "awesome"}),
-        awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+        awful.key({ modkey, "Shift" }, "F9", awesome.restart,
+                {description = "reload awesome", group = "awesome"}),
+        awful.key({ modkey, "sHIft"   }, "q", awesome.quit,
                 {description = "quit awesome", group = "awesome"}),
 
         awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
